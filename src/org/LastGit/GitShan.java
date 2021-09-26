82f4ca91e3c99646bfc963f25b83d6b3585dcfcc
shanmugam59 <shanmugam.avio@gmail.com>
2021-09-26 20:22:07 +0530

    Follow the last Git

---
+++ src/org/LastGit/GitShan.java
@@ -0,0 +1,18 @@
+package org.LastGit;
+
+public class GitShan {
+
+	private void Frastated() {
+		System.out.println("Because of Git");
+	}
+
+	private void Main() {
+		System.out.println("Following");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	}
+ 
+
